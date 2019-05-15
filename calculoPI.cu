4f7b949261c02c6a65ed4af964749b1a9cd99a3e
Freddy <facuellarg@unal.edu.co>
2019-05-15 01:16:46 -0500

    jmm

--- calculoPI.cu
+++ calculoPI.cu
@@ -13,7 +13,7 @@ calcularPi(float *pi, int operaciones)
 	for(int j = 0; j < operaciones; j++){
     i = i + j;
     if(i < 10){
-      printf("En %f valor %d\n",i,(2/((4*i + 1)*(4*i + 3))));
+      printf("En %f valor %f\n",i,(float)(2/((4*i + 1)*(4*i + 3))));
     }
     *pi = *pi + (2/((4*i + 1)*(4*i + 3)));
 	}
