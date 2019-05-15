1fc28dbc310c11603a3b3ee1d0a63fda1f5a756f
Freddy <facuellarg@unal.edu.co>
2019-05-15 01:04:14 -0500

    jmm

--- calculoPI.cu
+++ calculoPI.cu
@@ -14,7 +14,7 @@ calcularPi(float *pi, int operaciones)
   }
 	for(int j = 0; j < operaciones; j++){
     i = i + j;
-    *pi = *pi + (2/((4*(i+j) + 1)*(4*(i+j) + 3)));
+    *pi = *pi + (2/((4*(i) + 1)*(4*(i) + 3)));
 	}
     
 }
