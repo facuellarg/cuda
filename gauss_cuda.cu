29fc910ba0d72ab93079f9ab8530ac0dbae347ad
Freddy <facuellarg@unal.edu.co>
2019-05-31 01:46:34 -0500

    a

--- gauss_cuda.cu
+++ gauss_cuda.cu
@@ -57,7 +57,7 @@ blurEffect(double **h_kernel, int height, int width,  char *d_R,  char *d_G,char
                 {
                     int x = j - radius + l;
                     x = x < 0 ? 0 : x < width ? x : width - 1;
-                    printf("%f\n",d_R[y*width + x]);
+                    printf("%f\n", h_kernel[k][l]);
                     redTemp += d_R[y*width + x] * h_kernel[k][l];
                     
                     greenTemp += d_G[y*width + x] * h_kernel[k][l];
