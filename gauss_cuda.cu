e3dbd4a7c3941a33efa814339eaf61013f7acae7
Freddy <facuellarg@unal.edu.co>
2019-06-03 13:57:30 -0500

    try

--- gauss_cuda.cu
+++ gauss_cuda.cu
@@ -45,21 +45,34 @@ blurEffect(double *d_kernel, int height, int width,  unsigned char *d_R,  unsign
             double blueTemp = 0;
             double greenTemp = 0;
             double acum = 0;
-            for (int k = 0; k < kernelSize; k++ )
-            {
-                int y = (i - radius + k + height)%height;
-                for (int l = 0; l < kernelSize; l++)
-                {
-                    int x = (j - radius + l + width )% width;
-                    // x = x < 0 ? 0 : x < width ? x : width - 1;
-                    redTemp += d_R[y*width + x] * d_kernel[k*kernelSize + l];
-                    greenTemp += d_G[y*width + x] * d_kernel[k*kernelSize + l];
-                    blueTemp += d_B[y*width + x] * d_kernel[k*kernelSize + l];
-                    acum += d_kernel[k*kernelSize + l];
+            int count = 0;
+            // for (int k = 0; k < kernelSize; k++ )
+            // {
+            //     int y = (i - radius + k + height)%height;
+            //     for (int l = 0; l < kernelSize; l++)
+            //     {
+            //         int x = (j - radius + l + width )% width;
+            //         // x = x < 0 ? 0 : x < width ? x : width - 1;
+            //         redTemp += d_R[y*width + x] * d_kernel[k*kernelSize + l];
+            //         greenTemp += d_G[y*width + x] * d_kernel[k*kernelSize + l];
+            //         blueTemp += d_B[y*width + x] * d_kernel[k*kernelSize + l];
+            //         acum += d_kernel[k*kernelSize + l];
                     
+            //     }
+            // }
+
+            for (int fila = i - radio; fila < i + radio + (tamaño%2); fila++)
+            {
+                int y = fila < 0 ? 0 : fila < height ? fila : height - 1;
+                for (int columna = j - radio; columna < j + radio + (tamaño % 2); columna++)
+                {
+                    int x = columna < 0 ? 0 : columna < width ? columna : width - 1;
+                    redTemp += d_R[y*width + x] * d_kernel[count];
+                    greenTemp += d_G[y*width + x] * d_kernel[count];
+                    blueTemp += d_B[y*width + x] * d_kernel[count];
+                    acum += d_kernel[count++];
                 }
             }
-
             d_R[i*width + j] = redTemp/acum;
             d_G[i*width + j] = greenTemp/acum;
             d_B[i*width + j] = blueTemp/acum;
